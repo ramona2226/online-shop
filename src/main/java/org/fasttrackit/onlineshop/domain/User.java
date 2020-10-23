4979e174e328c6cd80189264a3fc58898e109be0
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-23 06:24:52 +0300

    ADDED USER ENTITY AND DATASOURCE CONFIGURATION

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/User.java
@@ -0,0 +1,56 @@
+package org.fasttrackit.onlineshop.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+
+    @NotNull
+    private String firstName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @NotNull
+    private String lastName;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
