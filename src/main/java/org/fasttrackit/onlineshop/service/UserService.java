cb3ad19cabb3ef1864651f979b457ff04d3056ac
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-23 20:28:12 +0300

    added create user method in user service

---
+++ src/main/java/org/fasttrackit/onlineshop/service/UserService.java
@@ -0,0 +1,37 @@
+package org.fasttrackit.onlineshop.service;
+
+import org.fasttrackit.onlineshop.domain.User;
+import org.fasttrackit.onlineshop.persistance.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+
+
+    //Inversion of CControl(IoC)- design pattern
+     private final UserRepository userRepository ;
+
+    //Dependency Injection
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser() {
+
+        LOGGER.info("Creating User: {}" , request);
+
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+ return userRepository.save(user);
+
+    }
+}
---
+++ src/main/java/org/fasttrackit/onlineshop/transfer/CreateUserRequest.java
@@ -0,0 +1,36 @@
+package org.fasttrackit.onlineshop.transfer;
+
+import javax.validation.constraints.NotNull;
+
+public class CreateUserRequest {
+
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String LastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUserRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                '}';
+    }
+}
